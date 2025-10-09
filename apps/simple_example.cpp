170cc0faa3f28f2d8f45552ec51c8a4d89c1f283
nico@thinkdev <nebokha@umich.edu>
2025-10-09 19:45:15 -0400

    [feat] add cmake project structure + updated gitignores acknowledging build directories, artifacts

---
+++ apps/simple_example.cpp
@@ -0,0 +1,15 @@
+//
+// Created by nico on 10/9/25.
+//
+// This is an example file as part of Modern-CMake
+
+#include <iostream>
+#include <sort-algs/lib.hpp>
+
+int main() {
+
+    std::cout << "Simple example C++ compiled correctly and ran." << std::endl;
+    std::cout << simple_lib_function() << std::endl;
+
+    return 0;
+}
---
+++ src/lib.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include "../include/sort-algs/lib.hpp"
+
+std::string simple_lib_function() {
+    return "Compiled in library";
+}
